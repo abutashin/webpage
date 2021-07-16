fed999225dcb8b07e0655df8a0bd95173c5900f0
MD. Abu Hussain (Tashin) <hussainabu@live.com>
2021-07-16 20:00:49 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function notify() {
+            alert("You're about to leave this page!!!");
+}
+
+function thankyou() {
+            alert("Thank you for the message. Though it doesn't work atm but InshAllah it'll be fixed soon :) ");
+}
